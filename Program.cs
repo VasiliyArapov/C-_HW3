e16c2e57c5d85962e92e5a6c1c0a4a574b4f6fad
Vasiliy <arapov95vasiliy@gmail.com>
2022-12-27 18:34:25 +0300

    Done my HW

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿// 1 Задача
+
+// Console.Write("Input five-digit number: ");
+// int num = int.Parse(Console.ReadLine()!);
+// string str_num = Convert.ToString(num);
+
+// if(str_num.Length == 5)
+// {
+//     if(str_num[0] == str_num[4] && str_num[1] == str_num[3])
+//     {
+//         Console.WriteLine("It's palindrome!");
+//     }
+//     else
+//     {
+//         Console.WriteLine("It's not palindrome...");
+//     }
+// }
+// else
+// {
+//     Console.WriteLine("Input five-difit number!!");
+// }
+
+// 2 Задача
+
+// Console.WriteLine("Input coordinates of the first point(x): ");
+// int x1 = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Input coordinates of the first point(y): ");
+// int y1 = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Input coordinates of the first point(z): ");
+// int z1 = int.Parse(Console.ReadLine()!);
+
+// Console.WriteLine("Input coordinates of the second point(x): ");
+// int x2 = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Input coordinates of the second point(y): ");
+// int y2 = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Input coordinates of the second point(z): ");
+// int z2 = int.Parse(Console.ReadLine()!);
+
+// double distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+
+// Console.WriteLine($"Distance betweeb points: {distance:f2}");
+
+// 3 Задача
+
+// Console.WriteLine("Input a number: ");
+// int num = int.Parse(Console.ReadLine()!);
+// for(int i = 1; i <= num; i++)
+// {
+//     Console.Write($"{Math.Pow(i, 3)} ");
+// }
